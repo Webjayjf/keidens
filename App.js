d027250b5956bf9dd684cd2f590626322d9994fa
Webjayjf <moletemolebatsiweb@gmail.com>
2024-10-01 14:56:00 +0200

    Update App.js

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.innerContainer}>
-        <Image source={require('./keilow.png')} style={styles.logo} resizeMode="contain" />
+        <Image source={require('./keilo.png')} style={styles.logo} resizeMode="contain" />
         
         <TextInput
           style={styles.input}
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#6a0dad',
+    backgroundColor: '#ffffff',
   },
   innerContainer: {
     width: '80%',
@@ -55,25 +55,25 @@ const styles = StyleSheet.create({
   },
   logo: {
     width: '80%',  // Adjust size as needed
-    marginBottom: 0,
+    marginBottom: 10,
   },
   input: {
     width: '100%',
     padding: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: '#6a0dad',
     borderRadius: 10,
     marginBottom: 20,
     backgroundColor: '#fff',
   },
   button: {
-    backgroundColor: '#ffffff', // Purple color
+    backgroundColor: '#6a0dad', // Purple color
     paddingVertical: 15,
     paddingHorizontal: 30,
     borderRadius: 10,
   },
   buttonText: {
-    color: '#6a0dad',
+    color: '#ffffff',
     fontSize: 16,
     fontWeight: 'bold',
   },
