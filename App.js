b6e75887f24575cd1029754054d50d4d8b42a4de
Webjayjf <moletemolebatsiweb@gmail.com>
2024-10-02 12:35:28 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,70 +1,30 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+// App.js
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import HomeScreen from './HomeScreen';
+import DetailScreen from './DetailScreen';
 
-const MyLayout = () => {
+const App = () => {
+  const [currentScreen, setCurrentScreen] = useState('Home');
+
+  const navigate = (screen) => {
+    setCurrentScreen(screen);
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      
-      {/* Image Section */}
-      <View style={styles.imageSection}>
-        <Image source={require('./keilo.png')} style={styles.image} />
-      </View>
-
-      
-
-      {/* Footer */}
-      <View style={styles.footer}>
-        
-        <Image source={require('./bbb.png')} style={styles.imaged}  />
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      {currentScreen === 'Home' && <HomeScreen navigate={navigate} />}
+      {currentScreen === 'Detail' && <DetailScreen navigate={navigate} />}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  header: {
-    padding: 20,
-    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
     alignItems: 'center',
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  imageSection: {
-    padding: 20,
-    alignItems: 'center',
-  },
-  image: {
-    width: 300,
-    height: 200,
-    resizeMode: 'contain',
-  },
-  imaged: {
-    width: 70,
-    height: 70,
-    resizeMode: 'contain',
-  },
-  content: {
-    padding: 20,
-  },
-  contentText: {
-    fontSize: 18,
-    color: '#333',
-  },
-  footer: {
-    padding: 20,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-  },
-  footerText: {
-    fontSize: 16,
-    color: '#777',
   },
 });
 
-export default MyLayout;
+export default App;
