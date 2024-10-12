186d4260521eb26404292ceb2e1a719822e4925f
Webjayjf <moletemolebatsiweb@gmail.com>
2024-10-12 23:47:42 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 // App.js
 import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import HomeScreen from './HomeScreen';
-import DetailScreen from './DetailScreen';
 
 const App = () => {
-  const [currentScreen, setCurrentScreen] = useState('Home');
-
-  const navigate = (screen) => {
-    setCurrentScreen(screen);
-  };
-
+  
   return (
     <View style={styles.container}>
-  
-      {currentScreen === 'Home' && <HomeScreen navigate={navigate} />}
-      {currentScreen === 'Detail' && <DetailScreen navigate={navigate} />}
+<Text>Hello hey</Text>
     </View>
   );
 };
